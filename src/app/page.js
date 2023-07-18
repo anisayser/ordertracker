477bb7750f89d3444dd41bc4b650d380917aef6c
Anis Ayser <anisayser1@gmail.com>
2023-07-18 11:50:43 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,75 @@
-import Image from 'next/image'
+"use client"
+import Image from "next/image";
+import orderTrackerImg from "../assets/images/orderTrackerImg3.png";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const handleLoad = () => {
+      Ordertracker({
+        id: "64b55f6f78eb3f2cb06dac08",
+        // Uncomment the next line & replace YOUR_TRACKING_NUMBER by a valid tracking number
+        // trackingNumber: "YOUR_TRACKING_NUMBER"
+      }).render("#ordertracker-widget");
+    };
+
+    const script = document.createElement("script");
+    script.src = "https://www.ordertracker.com/sdk.js";
+    script.onload = handleLoad;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+
+
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <>
+        <div className="w-full h-full lg:h-screen">
+          {/* <img src={bg1} className='w-full h-full' alt="" /> */}
+
+          {/* <Navigation /> */}
+          <div className="container px-5 lg:px-0  mx-auto pt-10">
+            <div className="grid grid-cols-2 gap-4 lg:gap-24 lg:h-[90vh]">
+              <div className="col-span-2 lg:col-span-1">
+                <h2 className="text-6xl font-bold leading-snug text-gray-900">
+                  Package tracking made easy, you just need a tracking number!
+                </h2>
+                <div className="pt-10" id="ordertracker-widget"></div>
+                {/* <div className='mt-10 flex items-center shadow-2xl'>
+                    <input type="text" className='p-8 w-full bg-white/90 rounded rounded-r-none backdrop-blur-xl text-black' value={trackingNumber} onChange={(e) => setTrackingNumber(e.target.value)} placeholder='Enter your tracking number' />
+                    <button onClick={handleTrack} className='bg-white/90 rounded rounded-l-none py-8 px-10 hover:bg-white duration-300 ease-linear relative z-10 group'>
+                        <span className="z-50">Search</span>
+                        <span className='w-[2px] h-full absolute bg-[#FC6747] top-0 left-0 -z-10 group-hover:w-full duration-300 ease-linear'></span>
+                    </button>
+                </div> */}
+                <div className="bg-black/10 mt-5 backdrop-blur-sm rounded p-5 pb text-gray-100">
+                  <p className="leading-7 tracking-wider text-gray-900">
+                    Track my package has never been that easy with this global
+                    parcel tracking tool allowing you to track parcels from any
+                    post office. A powerful universal parcel tracking system
+                    that will provide you with shipment info by tracking number
+                    lookup from any international courier such as China Post,
+                    Speedpak, 4PX Express, Amazon Logistics TBA, EMS ePacket,
+                    Dynamex, Aliexpress - Standard Shipping, Intelcom Express,
+                    Hermes, Deutsche Post - DHL Global mail - DHL Ecommerce,
+                    Yanwen, Canpar, Yunexpress, Pitney Bowes Inc - PBI, BNI -
+                    BNICA and many more!
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-span-2 lg:col-span-1">
+                <Image src={orderTrackerImg} className="xl:w-[800px]" alt="" />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </>
     </main>
-  )
+  );
 }
